592cbc042eecca27bcc7d7375312c02270af50d6
Du Nguyen <nguyend33@gmail.com>
2019-02-27 19:07:00 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # controller#action
+  root 'subs#index'
+  # get 'subs/index'
+
+  resources :subs do
+    resources :topics
+  end
+
+  # resources :subs
+  # get '/subs', to: 'subs#index'
+  # get '/subs/:id', to: 'subs#show'
+  # get '/subs/:id', to: 'subs#index'
+  # get '/subs/:id', to: 'subs#update'
+  # delete '/subs/:id', to "subs#destroy"
+
 end
